371ee420552d62858022feb9622f92c713600b6a
viktordanch <viktor.danch@gmail.com>
2016-04-15 21:46:39 +0300

    fix email configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
       address:              'smtp.gmail.com',
       port:                 587,
       domain:               'gmail.com',
-      user_name:            ENV['AWS_ACCESS_KEY_ID'],
-      password:             ENV['AWS_ACCESS_KEY_ID'],
+      user_name:            ENV['EMAIL'],
+      password:             ENV['EMAIL_PASSWORD'],
       authentication:       'plain',
       enable_starttls_auto: true  }
 
